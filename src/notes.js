3ab2660a7317be5a290a57854314406e1ed4edee
Ferdian Afza <farisafza201002@gmail.com>
2024-03-16 12:45:33 +0700

    first commit

---
+++ src/notes.js
@@ -0,0 +1,5 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+const notes = [];
+ 
+module.exports = notes;
---
+++ src/server.js
@@ -0,0 +1,25 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-trailing-spaces */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+        cors: {
+        origin: ['*'],
+        },
+    },
+  });
+
+  server.route(routes);
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
